fe126cfcea86c19615047f897f594db733e56a8b
Jakob Goebel <jakob.goebel@avenga.com>
2021-01-27 22:53:52 +0100

    #1 | unbreak build & tidy up gulpfile

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,26 +1,8 @@
-import {task, src, dest, parallel, series} from 'gulp'
-import * as del from 'del'
-import {createProject} from 'gulp-typescript'
+import {parallel, series, task} from 'gulp'
+import * as tasks from './gulp/tasks'
 
-const project = createProject('src/tsconfig.json')
+task('clean', tasks.clean)
+task('compile', tasks.compile)
+task('copy:static', tasks.copyStatic)
 
-task('clean', async () => {
-  del.sync('dist')
-  return Promise.resolve()
-})
-
-task('compile', () => {
-  return src('src/**/*.ts').pipe(project()).pipe(dest('dist/'))
-})
-
-task('copy', async () => {
-  src('README.md').pipe(dest('dist/'))
-  src('src/module.json').pipe(dest('dist/'))
-  src('src/lang/**').pipe(dest('dist/lang/'))
-  src('src/templates/**').pipe(dest('dist/templates/'))
-  src('src/styles/**').pipe(dest('dist/styles/'))
-  src('src/assets/**').pipe(dest('dist/assets/'))
-  return Promise.resolve()
-})
-
-task('build', series('clean', parallel('compile', 'copy')))
+task('build', series('clean', parallel('compile', 'copy:static')))
