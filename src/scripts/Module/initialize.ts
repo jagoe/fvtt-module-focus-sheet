4eff6d1d993dac9a124c41e7f74c29e51f2c331e
Jakob Goebel <jakob.goebel@avenga.com>
2021-01-29 18:41:44 +0100

    #3 | add tests

--- src/scripts/Module/initialize.ts
+++ src/scripts/Module/initialize.ts
@@ -2,7 +2,7 @@ import {focusCombatantSheet} from './focusCombatantSheet'
 import {registerSettings} from './registerSettings'
 
 export function initialize(): void {
-  Hooks.on('init', registerSettings)
+  Hooks.once('init', registerSettings)
 
   Hooks.on('updateCombat', focusCombatantSheet)
 }
---
+++ test/Module/registerSettings.test.ts
@@ -0,0 +1,55 @@
+import {SETTING_AUTO_OPEN, SETTING_AUTO_OPEN_POPOUT} from '@src/Popout/constants'
+
+import {MODULE_KEY} from '@src/Module/constants'
+import {cast} from '@util/cast'
+import {expect} from 'chai'
+import {registerSettings} from '@src/Module/registerSettings'
+
+describe('Module', () => {
+  describe('register settings', () => {
+    const settings: Record<string, unknown> = {}
+
+    before(() => {
+      global.game = cast({
+        settings: {
+          register(module: string, key: string, data: unknown) {
+            settings[`${module}.${key}`] = data
+          },
+        },
+        i18n: {
+          localize(str: string) {
+            return str
+          },
+        },
+      })
+    })
+
+    it("should register the 'auto open' setting", () => {
+      registerSettings()
+
+      const setting = settings[`${MODULE_KEY}.${SETTING_AUTO_OPEN}`]
+      expect(setting).to.eql({
+        name: `${MODULE_KEY}.${SETTING_AUTO_OPEN}.name`,
+        hint: `${MODULE_KEY}.${SETTING_AUTO_OPEN}.hint`,
+        scope: 'client',
+        config: true,
+        type: Boolean,
+        default: false,
+      })
+    })
+
+    it("should register the 'auto popup' setting", () => {
+      registerSettings()
+
+      const setting = settings[`${MODULE_KEY}.${SETTING_AUTO_OPEN_POPOUT}`]
+      expect(setting).to.eql({
+        name: `${MODULE_KEY}.${SETTING_AUTO_OPEN_POPOUT}.name`,
+        hint: `${MODULE_KEY}.${SETTING_AUTO_OPEN_POPOUT}.hint`,
+        scope: 'client',
+        config: true,
+        type: Boolean,
+        default: false,
+      })
+    })
+  })
+})
