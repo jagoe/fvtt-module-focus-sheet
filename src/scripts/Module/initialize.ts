f7643976f21f8a7f1ba9945888d160d5da714acf
Jakob Goebel <jakob.goebel@avenga.com>
2021-02-05 19:17:44 +0100

    refactor: re-initialize module on settings changes, so hooks can be registered based on settings
    Refs #7

--- src/scripts/Module/initialize.ts
+++ src/scripts/Module/initialize.ts
@@ -1,8 +1,18 @@
+import {Settings} from '../Settings'
 import {focusCombatantSheet} from './focusCombatantSheet'
 import {registerSettings} from '../Settings/registerSettings'
 
 export function initialize(): void {
-  Hooks.once('init', registerSettings)
+  const settings = Settings.GetInstance()
 
+  // load/reset settings
+  settings.Reset()
+
+  // de-register any existing hooks
+  Hooks.off('init', registerSettings)
+  Hooks.off('updateCombat', focusCombatantSheet)
+
+  // register hooks based on settings
+  Hooks.once('init', registerSettings)
   Hooks.on('updateCombat', focusCombatantSheet)
 }
