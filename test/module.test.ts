2d76008c4d1e2f537b00b8cbf6b760367760e666
Jakob Goebel <jakob.goebel@avenga.com>
2021-02-05 21:22:19 +0100

    test: test module bootstrap
    Refs #7

---
+++ test/module.test.ts
@@ -0,0 +1,29 @@
+import * as initialize from '@src/Module/initialize'
+
+import {SinonStub, createSandbox} from 'sinon'
+
+import {expect} from 'chai'
+
+describe('Bootstrap', () => {
+  const sandbox = createSandbox()
+  let initStub: SinonStub<[], void>
+
+  before(() => {
+    initStub = sandbox.stub(initialize, 'initialize')
+  })
+
+  afterEach(() => {
+    sandbox.reset()
+  })
+
+  after(() => {
+    sandbox.restore()
+  })
+
+  it('should initialize the module', () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require('@src/module')
+
+    expect(initStub.called).to.be.true
+  })
+})
