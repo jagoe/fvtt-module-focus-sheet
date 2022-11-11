92033ce864352e4e6f29aa7ca3b6e5ee47ce8ac7
Jonas Karlsson <xdy@users.noreply.github.com>
2022-11-11 17:35:07 +0100

    Add support for pf2e
    As pf2e has the same pc actor type as dnd5e, I just changed the condition.

--- src/scripts/System/getSystemPcActorTypes.ts
+++ src/scripts/System/getSystemPcActorTypes.ts
@@ -2,7 +2,7 @@ export function getSystemPcActorTypes(): string[] {
   const gameGlobal = game as unknown as Game
   const system = gameGlobal.system.id
 
-  if (system === 'dnd5e') {
+  if (system === 'dnd5e' || system === 'pf2e') {
     return ['character']
   }
 
